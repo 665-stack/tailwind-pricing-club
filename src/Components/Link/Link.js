d9b5d3291d469cfbc24dad5a9ab8bdf9549eb51d
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-08-22 13:40:29 +0600

    navbar style progressing

--- src/Components/Link/Link.js
+++ src/Components/Link/Link.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+//  bg-neutral-700/60 hover:bg-neutral-700/100 text-purple-300  shadow-md shadow-purple-400/50 rounded-md py-1 px-4
 const Link = ({ route }) => {
     const { name, link } = route;
     return (
-        <li className='mr-12 bg-neutral-700/60 hover:bg-neutral-700/100 text-gray-300 rounded-md py-1 px-4'>
+        <li className='my-2 md:mx-5   bg-neutral-700/60 hover:bg-neutral-700/100 text-purple-300 shadow shadow-purple-400/50 py-1 md:px-4 rounded-md'>
             <a href={link}>{name}</a>
 
         </li>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
-
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     const routes = [
         { id: 1, name: 'Home', link: '/.home' },
         { id: 2, name: 'Shop', link: '/.shop' },
@@ -10,13 +11,17 @@ const Navbar = () => {
         { id: 5, name: 'Contact', link: '/.contact' },
     ];
     return (
-        <nav >
-            <ul className='md:flex justify-center bg-neutral-900 py-4'>
+        <nav className='bg-neutral-900 py-1.5  mb:py-0'>
+            <div onClick={() => setOpen(!open)} className='w-7 h-7 ml-3  md:hidden text-purple-300'>
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+
+            <ul className={`md:flex pt-2 pb-5 md:pt-0 md:pb-0 px-20 w-full rounded-b-md justify-center bg-neutral-900/95 absolute md:static    duration-500 ease-out ${open ? 'top-9' : 'top-[-500px]'}`}>
                 {
                     routes.map(route => <Link route={route} key={route.id}></Link>)
                 }
             </ul>
-        </nav>
+        </nav >
     );
 };
 
